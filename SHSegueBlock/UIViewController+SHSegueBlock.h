68d3ad2675eaf4d385411e1e9cf14a4f00980151
Seivan Heidari <seivan@kth.se>
2013-05-16 01:30:30 +0200

    Added podspec

---
+++ SHSegueBlock/UIViewController+SHSegueBlock.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+SHSegueBlock.h
+//  Example
+//
+//  Created by Seivan Heidari on 5/16/13.
+//  Copyright (c) 2013 Seivan Heidari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (SHSegueBlock)
+
+@end
---
+++ SHSegueBlock/UIViewController+SHSegueBlock.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+SHSegueBlock.m
+//  Example
+//
+//  Created by Seivan Heidari on 5/16/13.
+//  Copyright (c) 2013 Seivan Heidari. All rights reserved.
+//
+
+#import "UIViewController+SHSegueBlock.h"
+
+@implementation UIViewController (SHSegueBlock)
+
+@end
