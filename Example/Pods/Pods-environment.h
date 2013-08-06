2f462395c37dcf8cc6329410a28f101d27fef3e4
Seivan Heidari <seivan@kth.se>
2013-08-06 02:44:39 +0200

    Add new pods with new test target.

--- Example/Pods/Pods-environment.h
+++ Example/Pods/Pods-environment.h
@@ -6,19 +6,6 @@
 // project.
 
 
-// KIF-next
-#define COCOAPODS_POD_AVAILABLE_KIF_next
-// This library does not follow semantic-versioning,
-// so we were not able to define version macros.
-// Please contact the author.
-// Version: 2.0.0pre4.
-
-// SHFastEnumerationProtocols
-#define COCOAPODS_POD_AVAILABLE_SHFastEnumerationProtocols
-#define COCOAPODS_VERSION_MAJOR_SHFastEnumerationProtocols 1
-#define COCOAPODS_VERSION_MINOR_SHFastEnumerationProtocols 2
-#define COCOAPODS_VERSION_PATCH_SHFastEnumerationProtocols 0
-
 // SHObjectUserInfo
 #define COCOAPODS_POD_AVAILABLE_SHObjectUserInfo
 #define COCOAPODS_VERSION_MAJOR_SHObjectUserInfo 1
@@ -30,10 +17,4 @@
 #define COCOAPODS_VERSION_MAJOR_SHSegueBlocks 1
 #define COCOAPODS_VERSION_MINOR_SHSegueBlocks 2
 #define COCOAPODS_VERSION_PATCH_SHSegueBlocks 0
-
-// SHTestCaseAdditions
-#define COCOAPODS_POD_AVAILABLE_SHTestCaseAdditions
-#define COCOAPODS_VERSION_MAJOR_SHTestCaseAdditions 1
-#define COCOAPODS_VERSION_MINOR_SHTestCaseAdditions 0
-#define COCOAPODS_VERSION_PATCH_SHTestCaseAdditions 0
 
