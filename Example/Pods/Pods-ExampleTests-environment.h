9d3b17e011bae63cc2bdbbe8e5de5afe969dc12b
Seivan Heidari <seivan@kth.se>
2013-08-06 02:26:29 +0200

    Update podspec.

--- Example/Pods/Pods-ExampleTests-environment.h
+++ Example/Pods/Pods-ExampleTests-environment.h
@@ -22,8 +22,8 @@
 // SHObjectUserInfo
 #define COCOAPODS_POD_AVAILABLE_SHObjectUserInfo
 #define COCOAPODS_VERSION_MAJOR_SHObjectUserInfo 1
-#define COCOAPODS_VERSION_MINOR_SHObjectUserInfo 0
-#define COCOAPODS_VERSION_PATCH_SHObjectUserInfo 2
+#define COCOAPODS_VERSION_MINOR_SHObjectUserInfo 1
+#define COCOAPODS_VERSION_PATCH_SHObjectUserInfo 0
 
 // SHSegueBlocks
 #define COCOAPODS_POD_AVAILABLE_SHSegueBlocks
